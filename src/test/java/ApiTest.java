b0acfa8251634d1724386f2ac6224974a7d62205
maxongen <maxongen41@gmail.com>
2025-08-19 16:41:22 +0530

    started studying restassured

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,5 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.Matchers.equalTo;
+
 public class ApiTest {
-    @Test
+    @Test(description = "test")
+    public void test(){
+       Response res= RestAssured.get("https://reqres.in/api/users");
+        System.out.println(res.asString());
+    }
 }
