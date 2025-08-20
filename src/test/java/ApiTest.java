7ddbf88d70a610ffc8fe7616e61c57d39fc686ba
maxongen <maxongen41@gmail.com>
2025-08-20 16:51:07 +0530

    added new code

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,14 +1,32 @@
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import static org.hamcrest.Matchers.equalTo;
 
 public class ApiTest {
     @Test(description = "test")
-    public void test(){
-       Response res= RestAssured.get("https://reqres.in/api/users");
+    public void test() throws IOException {
+       Response res=get("https://reqres.in/api/users");
         System.out.println(res.asString());
+
+        String latestVersion = given().when().get("https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_STABLE").
+                then().statusCode(200).extract().asString().trim();
+        System.out.println("latest version : "+latestVersion);
+        String os ="mac-x64";
+        String downloadURL=String.format("https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/%s/%s/chromedriver-%s.zip",latestVersion,os,os);
+        System.out.println("download URL : "+downloadURL);
+        Response res1=given().when().get(downloadURL).then().statusCode(200).extract().response();
+        byte[] fileBytes=res1.asByteArray();
+        String filename="chromedriver-" + os + "-" + latestVersion + ".zip";
+        try(FileOutputStream fos=new FileOutputStream(filename)){
+            fos.write(fileBytes);
+        }
+        System.out.println("✅ ChromeDriver downloaded: " + filename);
     }
 }
